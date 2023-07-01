40317af6b1019ad8b0eebe7660511d3d45a3b54c
ismailgulbay <m.ismailgulbay@gmail.com>
2023-07-01 10:22:21 +0300

    Create discountApi and IDiscountService.

---
+++ Services/Discount/FreeCourse.Services.Discount/Services/IDiscountService.cs
@@ -0,0 +1,14 @@
+﻿using FreeCourse.Shared.Dtos;
+
+namespace FreeCourse.Services.Discount.Services
+{
+    public interface IDiscountService
+    {
+        Task<Response<List<Models.Discount>>> GetAll();
+        Task<Response<Models.Discount>> GetById(int id);
+        Task<Response<Models.Discount>> Save(Models.Discount discount);
+        Task<Response<Models.Discount>> Update(Models.Discount discount);
+        Task<Response<Models.Discount>> Delete(int id);
+        Task<Response<Models.Discount>> GetByCodeAndUserId(string code, string userId);
+    }
+}
